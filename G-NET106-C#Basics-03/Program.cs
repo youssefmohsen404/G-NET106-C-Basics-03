95945850a1e11af32e7045fe96704096578bf38a
youss <youssmohss111@gmail.com>
2026-08-29 17:58:10 +0300

    Question2

--- G-NET106-C#Basics-03/Program.cs
+++ G-NET106-C#Basics-03/Program.cs
@@ -19,6 +19,25 @@
             }*/
 
             #endregion
+            #region Question2
+            //Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code",
+            //"Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
+
+            //string title = "Refactoring";
+          /*  Console.WriteLine("please enter a string");
+            string title = Console.ReadLine();
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("great choice");
+                    break;
+                case "Refactoring":
+                    Console.WriteLine("Nice pick");
+                    break;
+                default: Console.WriteLine("Never heard of it");
+                    break;
+            }*/
+            #endregion
 
         }
     }
